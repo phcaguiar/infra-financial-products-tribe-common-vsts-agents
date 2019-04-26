df57b44357513073966f7de70438c92b128ced9a
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2019-04-26 03:49:42 +0000

    add icmp firewall rule

--- src/terraform/common/files/winrm.ps1
+++ src/terraform/common/files/winrm.ps1
@@ -1,5 +1,6 @@
 Write-Host "Open WinRM Firewall Port"
 netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-In)" dir=in action=allow protocol=TCP localport=5985
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
 
 Write-Host "Enable PSRemoting"
 Enable-PSRemoting -Force
